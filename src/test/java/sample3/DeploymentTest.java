c41b545384a601982c8d8ce3e17c00daa2807cc4
ADMIN <ADMIN@DESKTOP-VB164NU>
2022-07-20 14:52:16 +0530

    commit

---
+++ src/test/java/sample3/DeploymentTest.java
@@ -0,0 +1,13 @@
+package sample3;
+
+import org.testng.annotations.Test;
+
+public class DeploymentTest {
+	
+	@Test
+	public void Smokecontact()
+	{
+		System.out.println("Insidfdhghgfgde svbhgdhjfgmoke create defefe  contact....");
+	}
+
+}
